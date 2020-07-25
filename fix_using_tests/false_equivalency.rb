e9490258074479c108c3976892b950042d824cc4
Mike Archer <mlarcher531@gmail.com>
2020-07-25 14:47:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,9 +1,7 @@
-# don't forget to add: require 'pry'
-
+require 'pry'
 def get_user_input
   gets.chomp
 end
-
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
@@ -15,6 +13,7 @@ def selection(num)
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
+     binding.pry 
   end
 end
 

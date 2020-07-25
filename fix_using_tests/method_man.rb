fa6a119115f9f8c1981f5d078f003cd8a7f8f566
Mike Archer <mlarcher531@gmail.com>
2020-07-25 15:06:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
@@ -6,6 +5,4 @@ end
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
-binding.pry
-puts "hello"
 end
